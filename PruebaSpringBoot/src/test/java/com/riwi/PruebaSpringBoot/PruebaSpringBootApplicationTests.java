59c2c4a94c9b18f910b76639411460b770e017e8
miguelangito <miguelangelespinalospina@gmail.com>
2024-06-04 06:28:03 -0500

    primer commit initializr vacio

---
+++ PruebaSpringBoot/src/test/java/com/riwi/PruebaSpringBoot/PruebaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.PruebaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
